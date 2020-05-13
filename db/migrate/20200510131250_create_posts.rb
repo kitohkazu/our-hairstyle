9a81158704638bbd2f980ed61cac7fc812002529
kitohkazu <64017216+kitohkazu@users.noreply.github.com>
2020-05-13 15:47:41 +0900

    rails db migrate commit
    ・imagesテーブルとpostsテーブルをrails db:migrateすることにより作成した

--- db/migrate/20200510131250_create_posts.rb
+++ db/migrate/20200510131250_create_posts.rb
@@ -1,9 +1,9 @@
 class CreatePosts < ActiveRecord::Migration[5.2]
   def change
-    create_table :posts do |t|
+    create_table :posts, id: :integer do |t|
       t.string :comment
-      t.references :user, foreign_key: true
-      t.references :group, foreign_key: true
+      t.integer :user_id, foreign_key: true
+      t.integer :group_id, foreign_key: true
       t.timestamps
     end
   end
--- db/migrate/20200513031330_create_images.rb
+++ db/migrate/20200513031330_create_images.rb
@@ -1,8 +1,8 @@
 class CreateImages < ActiveRecord::Migration[5.2]
   def change
-    create_table :images do |t|
+    create_table :images, id: :integer do |t|
       t.string :src
-      t.references :post, foreign_key: true
+      t.integer :post_id, foreign_key: true
       t.timestamps
     end
   end
