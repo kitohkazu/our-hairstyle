361a76d05fcacf5d60944286d50fb6ec763bfe7b
kitohkazu <64017216+kitohkazu@users.noreply.github.com>
2020-06-08 17:00:14 +0900

    バリデーションの追加

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,4 +1,5 @@
 class Image < ApplicationRecord
   mount_uploader :src, ImageUploader
+  validates :src, presence: true
   belongs_to :post
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
+  validates :images, presence: true
   has_many :images
   has_many :likes
   belongs_to :user
