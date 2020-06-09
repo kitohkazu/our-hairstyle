46ed78a5e90daa2784c4a7519a03f983144fd856
kitohkazu <64017216+kitohkazu@users.noreply.github.com>
2020-06-09 10:41:41 +0900

    Favoliteモデルの作成

---
+++ app/models/favolite.rb
@@ -0,0 +1,2 @@
+class Favolite < ApplicationRecord
+end
---
+++ test/models/favolite_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class FavoliteTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
