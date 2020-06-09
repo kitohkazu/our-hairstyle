5174b1e6c6ee3f851123111790430d74d331565c
kitohkazu <64017216+kitohkazu@users.noreply.github.com>
2020-06-09 18:44:27 +0900

    favoriteモデルのアソシエーション追加とモデル名訂正

--- app/models/favolite.rb
+++
@@ -1,2 +0,0 @@
-class Favolite < ApplicationRecord
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_09_013525) do
+ActiveRecord::Schema.define(version: 2020_06_09_055514) do
 
-  create_table "favolites", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "favorites", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id", null: false
     t.integer "post_id", null: false
     t.datetime "created_at", null: false
