e0c29c48e42441d23a9d9fa2d6bccf5d857dec09
kitohkazu <64017216+kitohkazu@users.noreply.github.com>
2020-06-09 18:49:31 +0900

    favoritesコントローラーの作成

---
+++ app/controllers/favorites_controller.rb
@@ -0,0 +1,20 @@
+class FavoritesController < ApplicationController
+
+  def create
+    favorite = Favorite.create(user_id: current_user.id, post_id: params[:post_id])
+    if favorite.save
+      redirect_to post_path(params[:post_id])
+    end
+  end
+
+  def destroy
+    favorite = Favorite.find_by(user_id: current_user.id, post_id: params[:post_id])
+    if favorite.destroy
+      redirect_to post_path(params[:post_id])
+    end
+  end
+
+  def index
+  end
+
+end
---
+++ app/helpers/favolites_helper.rb
@@ -0,0 +1,2 @@
+module FavolitesHelper
+end
