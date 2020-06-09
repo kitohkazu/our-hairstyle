737e261d7d7d149f6fa130dfb38263bcea674325
kitohkazu <64017216+kitohkazu@users.noreply.github.com>
2020-06-09 09:51:42 +0900

    レイアウトや変数定義の微修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   def index
     @posts = Post.includes(:images)
     @posts_men = Post.where(gender: 0).includes(:images)
-    @posts_women = Post.where(gender: 1).includes(:images)
+    @posts_lady = Post.where(gender: 1).includes(:images)
   end
 
   def new
