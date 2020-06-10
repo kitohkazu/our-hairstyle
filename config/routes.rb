ec22550bfc7abc9f6074b857ddc31e160746e0a8
kitohkazu <64017216+kitohkazu@users.noreply.github.com>
2020-06-10 18:18:36 +0900

    お気に入り一覧ページのルーティング定義

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "posts#index"
   resources :posts do
-    resources :favorites, only: [:create, :destroy, :index]
+    resources :favorites, only: [:create, :destroy]
   end
+  resources :favorites, only: [:index]
 end
