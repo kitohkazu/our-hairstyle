71369835b32636046257a183440f1ead3a256f1a
kitohkazu <64017216+kitohkazu@users.noreply.github.com>
2020-06-18 08:29:21 +0900

    日時表示を日本時間に修正

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ Bundler.require(*Rails.groups)
 module OurHairstyle
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
+    config.time_zone = 'Tokyo'
     config.load_defaults 5.2
     config.i18n.default_locale = :ja
     # Settings in config/environments/* take precedence over those specified here.
